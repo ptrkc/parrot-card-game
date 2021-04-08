07461a46c9637eb90261610bb6e365ef99a650a4
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-07 23:15:56 -0300

    added function to flip cards triggered by an EventListener

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let amountOfCards = 0;
+let amountOfCards = 14;
 
 while (amountOfCards > 14 || amountOfCards < 4 || amountOfCards % 2 !== 0) {
     askAmount()
@@ -6,3 +6,18 @@ while (amountOfCards > 14 || amountOfCards < 4 || amountOfCards % 2 !== 0) {
 function askAmount() {
     amountOfCards = prompt("Com quantas cartas você deseja jogar?");
 }
+
+const cardsList = document.querySelectorAll(".card")
+document.querySelector("#content").addEventListener('click', userClick);
+
+function userClick(event) {
+    const clickedCard = event.target.parentNode
+    if (clickedCard.classList.value === "card") {
+        clickedCard.classList.toggle("flipped")
+    } else {
+        console.log("nope")
+    }
+}
+
+
+document.querySelector("#content")
