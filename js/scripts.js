ea9bb9a6af49e1597a9b2b1391abef3cf66d3dd2
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-08 00:06:42 -0300

    added setCards() to set the number of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
-let amountOfCards = 14;
-
-while (amountOfCards > 14 || amountOfCards < 4 || amountOfCards % 2 !== 0) {
-    askAmount()
-}
-function askAmount() {
-    amountOfCards = prompt("Com quantas cartas você deseja jogar?");
-}
-
-const cardsList = document.querySelectorAll(".card")
+let amountOfCards = 10;
 document.querySelector("#content").addEventListener('click', userClick);
+askAmount();
+
+function askAmount() {
+    while (amountOfCards > 14 || amountOfCards < 4 || amountOfCards % 2 !== 0) {
+        amountOfCards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
+    }
+    setCards();
+}
+
+function setCards() {
+    for (let i = 0; i < 14 - amountOfCards; i++) {
+        document.querySelector(".card").remove()
+    }
+}
 
 function userClick(event) {
     const clickedCard = event.target.parentNode
     if (clickedCard.classList.value === "card") {
-        clickedCard.classList.toggle("flipped")
+        clickedCard.classList.toggle("flipped");
     } else {
-        console.log("nope")
+        console.log("nope");
     }
 }
-
-
-document.querySelector("#content")
