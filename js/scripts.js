c8f0cbfd94da38c1cef21a19b18d7aaebb125dc5
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-08 03:45:24 -0300

    added stopwatch and related functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,11 @@ let lastParrot = "";
 let blocked = false;
 let totalMoves = 0;
 let flippedCards = 0;
+let gameStart = 0;
+let seconds = 0;
+let interval;
 const possibleCards = ["bob", "explode", "fiesta", "metal", "revertit", "triplets", "unicorn"]
+const stopwatch = document.getElementById('stopwatch');
 
 document.querySelector("#content").addEventListener('click', userClick);
 
@@ -16,7 +20,25 @@ function askAmount() {
         amountOfCards = parseInt(prompt("Com quantas cartas você deseja jogar?(Números pares de 4 a 14)"));
     }
     setCards();
+    startStopwatch();
 }
+
+function startStopwatch() {
+    gameStart = Date.now();
+    interval = setInterval(updateSeconds, 1000);
+}
+
+function updateSeconds() {
+    seconds = Math.floor((Date.now() - gameStart) / 1000);
+    stopwatch.innerHTML = seconds;
+};
+
+function stopStopwatch() {
+    updateSeconds();
+    clearInterval(interval);
+}
+
+
 
 function setCards() {
     randomizeCards();
@@ -26,7 +48,7 @@ function setCards() {
     <li class="card ${chosenCards[i]}">
         <div class="front-face face"></div>
         <div class="back-face face"></div>
-    </li>`
+    </li>`;
     }
 }
 function randomizeCards() {
@@ -40,7 +62,7 @@ function comparador() {
 }
 
 function userClick(event) {
-    const clickedCard = event.target.parentNode
+    const clickedCard = event.target.parentNode;
     const clickedParrot = clickedCard.classList[1]
     if (clickedCard.classList[0] === "card" &&
         !clickedCard.classList.contains("flipped") &&
@@ -71,7 +93,8 @@ function userClick(event) {
 }
 
 function gameOver() {
-    alert(`Você ganhou em ${totalMoves} jogadas!`);
+    stopStopwatch();
+    alert(`Você ganhou em ${totalMoves} jogadas e ${seconds} segundos!`);
     let validAnswer = false;
     while (validAnswer !== true) {
         playAgain = prompt("Você quer jogar novamente?(sim ou não)");
@@ -79,6 +102,8 @@ function gameOver() {
             validAnswer = true;
             totalMoves = 0;
             flippedCards = 0;
+            seconds = 0;
+            stopwatch.innerHTML = "0";
             askAmount();
         } else if (playAgain === "não" || playAgain === "nao" || playAgain === "n") {
             validAnswer = true;
