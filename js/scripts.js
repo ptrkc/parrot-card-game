1d0d2b1c46ebde76bed81b7fbe5993d9d82b3964
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-08 00:47:20 -0300

    added setCardType() to add the parrots

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let amountOfCards = 10;
 const possibleCards = ["bob", "explode", "fiesta", "metal", "revertit", "triplets", "unicorn"]
-
+let chosenCards = [];
 document.querySelector("#content").addEventListener('click', userClick);
 askAmount();
 
@@ -8,21 +8,29 @@ function askAmount() {
     while (amountOfCards > 14 || amountOfCards < 4 || amountOfCards % 2 !== 0) {
         amountOfCards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
     }
-    setCards();
+    setAmountOfCards();
 }
 
-function setCards() {
+function setAmountOfCards() {
     for (let i = 0; i < 14 - amountOfCards; i++) {
         document.querySelector(".card").remove()
     }
     randomizeCards();
+    setCardType();
+}
+
+function setCardType() {
+    const cardList = document.querySelectorAll(".card")
+    for (let i = 0; i < cardList.length; i++) {
+        cardList[i].classList.add(chosenCards[i]);
+    }
 }
 
 function randomizeCards() {
-    let chosencards = possibleCards.sort(comparador).slice(0, amountOfCards / 2);
-    chosencards = chosencards.concat(chosencards);
-    chosencards = chosencards.sort(comparador);
-    console.log(chosencards);
+    chosenCards = possibleCards.sort(comparador).slice(0, amountOfCards / 2);
+    chosenCards = chosenCards.concat(chosenCards);
+    chosenCards = chosenCards.sort(comparador);
+    console.log(chosenCards);
 }
 
 function comparador() {
