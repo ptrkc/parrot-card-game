8a4a29eaa40da95a7fc631118e11481304d45080
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-08 00:38:28 -0300

    added randomizeCards() and moved .flipped to .card

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 let amountOfCards = 10;
+const possibleCards = ["bob", "explode", "fiesta", "metal", "revertit", "triplets", "unicorn"]
+
 document.querySelector("#content").addEventListener('click', userClick);
 askAmount();
 
@@ -13,12 +15,24 @@ function setCards() {
     for (let i = 0; i < 14 - amountOfCards; i++) {
         document.querySelector(".card").remove()
     }
+    randomizeCards();
+}
+
+function randomizeCards() {
+    let chosencards = possibleCards.sort(comparador).slice(0, amountOfCards / 2);
+    chosencards = chosencards.concat(chosencards);
+    chosencards = chosencards.sort(comparador);
+    console.log(chosencards);
+}
+
+function comparador() {
+    return Math.random() - 0.5;
 }
 
 function userClick(event) {
     const clickedCard = event.target.parentNode
-    if (clickedCard.classList.value === "card") {
-        clickedCard.classList.toggle("flipped");
+    if (clickedCard.classList[0] === "card" && !clickedCard.classList.contains("flipped")) {
+        clickedCard.classList.add("flipped");
     } else {
         console.log("nope");
     }
